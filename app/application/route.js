e83edb8bc5aedf5a31c861f04aa26f0ee14c66ba
Dan Monroe <daniel.monroe@cardinalhealth.com>
2016-05-11 23:23:25 -0400

    Commented out initial requests for non phoenix data

--- app/application/route.js
+++ app/application/route.js
@@ -5,16 +5,17 @@ export default Ember.Route.extend({
         var request = {
             contests: this.store.findAll('contest'),
             aircrafttypes: this.store.findAll('aircrafttype'),
-            pilotclasses: this.store.findAll('pilotclass'),
-            maneuversets: this.store.findAll('maneuverset'),
-            maneuvers: this.store.findAll('maneuver'),
-            registrations: this.store.findAll('registration'),
-            rounds: this.store.findAll('round'),
-            maneuverscores: this.store.findAll('maneuverscore'),
-            roundscores: this.store.findAll('roundscore'),
+
+            //pilotclasses: this.store.findAll('pilotclass'),
+            //maneuversets: this.store.findAll('maneuverset'),
+            //maneuvers: this.store.findAll('maneuver'),
+            //registrations: this.store.findAll('registration'),
+            //rounds: this.store.findAll('round'),
+            //maneuverscores: this.store.findAll('maneuverscore'),
+            //roundscores: this.store.findAll('roundscore'),
 
 
-            scores: this.store.findAll('score'),
+            //scores: this.store.findAll('score'),
 
             pilots: this.store.findAll('pilot')
 
